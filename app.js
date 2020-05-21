88fcabade5dc957bf3170744c1d3217940c575d7
Daniel Kim <dahnielkim@gmail.com>
2020-05-21 10:09:20 -0700

    Updated formatting

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let pageNumber = document.getElementById('page-number');
 let total = document.getElementById('total');
 let skipBar = document.getElementById('page-skip');
 
-document.querySelector("#file-input").addEventListener('change', function () {
+document.querySelector('#file-input').addEventListener('change', function () {
     let all_files = this.files;
     if (all_files.length == 0) {
         return;
@@ -18,32 +18,32 @@ document.querySelector("#file-input").addEventListener('change', function () {
         return;
     }
 
-    let max_size_allowed = 2 * 1024 * 1024
+    let max_size_allowed = 2 * 1024 * 1024;
     if (file.size > max_size_allowed) {
         alert('Error : Exceeded size 2MB');
         return;
     }
 
-    let fr = new FileReader()
+    let fr = new FileReader();
     fr.addEventListener('load', function (e) {
         urlList = [];
         let text = e.target.result;
-        parseText(text)
-        loadUrl()
-        total.innerHTML = urlList.length
+        parseText(text);
+        loadUrl();
+        total.innerHTML = urlList.length;
     });
-    
+
     fr.readAsText(file);
 });
 
 function handleClick(_value) {
     let tempIndex;
-    if(_value) {
-        tempIndex = Math.min(currentIndex+1, urlList.length-1);
+    if (_value) {
+        tempIndex = Math.min(currentIndex + 1, urlList.length - 1);
     } else {
-        tempIndex = Math.max(currentIndex-1, 0);
+        tempIndex = Math.max(currentIndex - 1, 0);
     }
-    if(tempIndex == currentIndex) {
+    if (tempIndex == currentIndex) {
         return;
     }
     currentIndex = tempIndex;
@@ -51,69 +51,71 @@ function handleClick(_value) {
 }
 
 function loadUrl() {
-    if(!urlList[currentIndex]) {
+    if (!urlList[currentIndex]) {
         return;
     }
-    pages.style.display = "block";
-    skipBar.style.display = "flex";
-    pageNumber.innerHTML = currentIndex+1;
+    pages.style.display = 'block';
+    skipBar.style.display = 'flex';
+    pageNumber.innerHTML = currentIndex + 1;
     // frame.src = urlList[currentIndex];
-    let strWindowFeatures = "location=yes,height=900,width=1000,scrollbars=yes,status=yes";
-    window.open(urlList[currentIndex], "popup", strWindowFeatures)
-    
+    let strWindowFeatures =
+        'location=yes,height=900,width=1000,scrollbars=yes,status=yes';
+    window.open(urlList[currentIndex], 'popup', strWindowFeatures);
 }
 
 function skipTo() {
     let skipInput = document.getElementById('skip-input');
     let value = skipInput.value;
-    let page = parseInt(value)
-    if(isNaN(page)) {
+    let page = parseInt(value);
+    if (isNaN(page)) {
         alert('not a number');
         return;
     }
-    if(page < 0 || page > urlList.length) {return;}
-    currentIndex = page-1;
+    if (page < 0 || page > urlList.length) {
+        return;
+    }
+    currentIndex = page - 1;
     loadUrl();
-    skipInput.vaule = "";
+    skipInput.vaule = '';
 }
 
 function parseText(_text) {
-    _text = _text.replace(/\"/g, "");
+    _text = _text.replace(/\"/g, '');
     // _text = _text.replace(/\n/g, "");
-    _text = _text.replace(/" "/g, "");
+    _text = _text.replace(/" "/g, '');
     let lines = _text.split('\n');
-    lines.forEach(line => {
-        let urls = line.split(",");
-        urls.forEach(url => {
-            if (url != "") {
-                urlList.push(url)
+    lines.forEach((line) => {
+        let urls = line.split(',');
+        urls.forEach((url) => {
+            if (url != '') {
+                urlList.push(url);
             }
-        })
-    })
+        });
+    });
 }
 
 function handleTextArea() {
     urlList = [];
     currentIndex = 0;
     let skipInput = document.getElementById('skip-input');
-    skipInput.value = "";
+    skipInput.value = '';
     let ta = document.getElementById('text-area-input');
     let text = ta.value;
     parseText(text);
-    loadUrl()
-    total.innerHTML = urlList.length
+    loadUrl();
+    total.innerHTML = urlList.length;
     toggleInputButtons(false);
-    ta.value = "";
+    ta.value = '';
 }
 
 function toggleInputButtons(value) {
     let textArea = document.getElementById('text-input');
     let buttons = document.getElementById('input-buttons');
-    if(value) {
-        textArea.style.display = "flex";
-        buttons.style.display = "none";
+    if (value) {
+        textArea.style.display = 'flex';
+        buttons.style.display = 'none';
     } else {
-        textArea.style.display = "none";
-        buttons.style.display = "flex";
+        textArea.style.display = 'none';
+        buttons.style.display = 'flex';
     }
 }
